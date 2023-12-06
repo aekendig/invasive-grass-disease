dba8a7d8050fbe4675dd67bc2ceabbcc1536a6eb
Amy Kendig <aekendig@gmail.com>
2023-12-06 09:18:39 -0600

    revise germination figures

--- code/model-fitting/germination_2018_2019_density_exp.R
+++ code/model-fitting/germination_2018_2019_density_exp.R
@@ -37,6 +37,9 @@ mod_check_fun <- function(mod){
   
 }
 
+# figure settings
+source("code/figure-prep/figure_settings.R")
+
 
 #### edit data ####
 
@@ -96,57 +99,43 @@ mvGermD1Mod <- brm(data = mvGermD1Dat, family = binomial,
 mod_check_fun(mvGermD1Mod)
 save(mvGermD1Mod, file = "output/mv_germination_fungicide_model_2018_density_exp.rda")
 
-#### start here ####
-# can import models from microstegium-bipolaris folder and resave
-# note name change on next one
-
-# model
-mvGermFungD1Mod <- brm(data = mvGermD1Dat, family = binomial,
+# seed infection model
+mvGermInfD1Mod <- brm(data = mvGermD1Dat, family = binomial,
                    germination_final | trials(seeds) ~ prop_dark + prop_light + (1|plotf),
                    prior <- c(prior(normal(0, 10), class = "Intercept"),
                               prior(normal(0, 10), class = "b")), # use default for sigma
                    iter = 6000, warmup = 1000, chains = 3, cores = 3)
-save(mvGermFungD1Mod, file = "output/mv_germination_infection_model_2018_density_exp.rda")
-# mod_check_fun(mvGermD1Mod)
-# prop dark decreases germination
-# prop light increases germination
+mod_check_fun(mvGermInfD1Mod)
+save(mvGermInfD1Mod, file = "output/mv_germination_infection_model_2018_density_exp.rda")
 
-
-
-mvPropDarkMod2 <- brm(data = mvGermD1Dat, family = binomial,
+# proportion of seeds with dark infection
+mvPropDarkMod <- brm(data = mvGermD1Dat, family = binomial,
                      seeds_dark | trials(seeds) ~ fungicide + (1|plotf),
                      prior <- c(prior(normal(0, 10), class = "Intercept"),
                                 prior(normal(0, 10), class = "b")), # use default for sigma
                      iter = 6000, warmup = 1000, chains = 3, cores = 3)
-# mod_check_fun(mvPropDarkMod2)
-# fungicide decreases prop dark
+mod_check_fun(mvPropDarkMod)
+save(mvPropDarkMod, file = "output/mv_seed_infection_dark_model_2018_density_exp.rda")
 
-mvPropLightMod2 <- brm(data = mvGermD1Dat, family = binomial,
+# proportion of seeds with light infection
+mvPropLightMod <- brm(data = mvGermD1Dat, family = binomial,
                       seeds_light | trials(seeds) ~ fungicide + (1|plotf),
                       prior <- c(prior(normal(0, 10), class = "Intercept"),
                                  prior(normal(0, 10), class = "b")), # use default for sigma
                       iter = 6000, warmup = 1000, chains = 3, cores = 3)
-# mod_check_fun(mvPropLightMod2)
-# fungicide doesn't affect prop light
-
-# save
-
-
-save(mvPropDarkMod2, file = "output/mv_seed_infection_dark_fungicide_model_2018_density_exp.rda")
-save(mvPropLightMod2, file = "output/mv_seed_infection_light_fungicide_model_2018_density_exp.rda")
+mod_check_fun(mvPropLightMod)
+save(mvPropLightMod, file = "output/mv_seed_infection_light_model_2018_density_exp.rda")
 
 # load
-load("output/mv_germination_infection_model_2018_density_exp.rda")
 load("output/mv_germination_fungicide_model_2018_density_exp.rda")
-load("output/mv_seed_infection_dark_fungicide_model_2018_density_exp.rda")
-load("output/mv_seed_infection_light_fungicide_model_2018_density_exp.rda")
+load("output/mv_germination_infection_model_2018_density_exp.rda")
+load("output/mv_seed_infection_dark_model_2018_density_exp.rda")
+load("output/mv_seed_infection_light_model_2018_density_exp.rda")
 
 # tables
-write_csv(tidy(mvGermD1Mod), "output/mv_germination_infection_model_2018_density_exp.csv")
-write_csv(tidy(mvPropDarkMod2), "output/mv_seed_infection_dark_fungicide_model_2018_density_exp.csv")
-
-# save corresponding data
-write_csv(mvGermD1Dat, "output/mv_germination_fungicide_model_data_2018_density_exp.csv")
+write_csv(tidy(mvGermD1Mod), "output/mv_germination_fungicide_model_2018_density_exp.csv")
+write_csv(tidy(mvGermInfD1Mod), "output/mv_germination_infection_model_2018_density_exp.csv")
+write_csv(tidy(mvPropDarkMod), "output/mv_seed_infection_dark_model_2018_density_exp.csv")
 
 
 #### Ev models ####
@@ -175,81 +164,71 @@ save(evGermMod, file = "output/ev_germination_fungicide_model_2018_2019_density_
 # load
 load("output/ev_germination_fungicide_model_2018_2019_density_exp.rda")
 
-# save corresponding data
-write_csv(evGermDat2, "output/ev_germination_fungicide_model_data_2018_2019_density_exp.rda")
+# table
+write_csv(tidy(evGermMod), "output/ev_germination_fungicide_model_2018_2019_density_exp.csv")
 
 
-#### figure ####
+#### fungicide effect figure ####
 
-# figure settings
-fig_theme <- theme_bw() +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.text = element_text(size = 7, color = "black"),
-        axis.title = element_text(size = 7, color = "black"),
-        legend.text = element_text(size = 7),
-        legend.title = element_text(size = 7),
-        legend.background = element_blank(),
-        legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.margin = margin(-0.3, 0, -0.1, 0, unit = "cm"),
-        legend.box = "vertical",
-        plot.title = element_text(size = 7, hjust = 0.5))
+# posterior draws
+mvGermD1Draws <- as_draws_df(mvGermD1Mod)
+evGermDraws <- as_draws_df(evGermMod)
 
-# simulated data
-mvFungSim <- tibble(fungicide = c(0, 1)) %>%
-  mutate(plotf = "A",
-         treatment = c("control", "fungicide"),
-         seeds = round(mean(mvGermD1Dat$seeds))) %>%
-  mutate(prop_dark = fitted(mvPropDarkMod2, newdata = ., allow_new_levels = T)[, "Estimate"]/seeds,
-         lower = fitted(mvPropDarkMod2, newdata = ., allow_new_levels = T)[, "Q2.5"]/seeds,
-         upper = fitted(mvPropDarkMod2, newdata = ., allow_new_levels = T)[, "Q97.5"]/seeds)
+# combine
+germDraws <- tibble(sp = "M. vimineum",
+                    fung_eff = mvGermD1Draws$b_fungicide) %>%
+  full_join(tibble(sp = "E. virginicus",
+                   fung_eff = evGermDraws$b_fungicide))
 
-mvGermSim <- tibble(prop_dark = seq(min(mvGermD1Dat$prop_dark), max(mvGermD1Dat$prop_dark), length.out = 100)) %>%
-  mutate(plotf = "A",
-         seeds = round(mean(mvGermD1Dat$seeds)),
-         prop_light = mean(mvGermD1Dat$prop_light)) %>%
-  mutate(prop_germ = fitted(mvGermD1Mod, newdata = ., allow_new_levels = T)[, "Estimate"]/seeds,
-         lower = fitted(mvGermD1Mod, newdata = ., allow_new_levels = T)[, "Q2.5"]/seeds,
-         upper = fitted(mvGermD1Mod, newdata = ., allow_new_levels = T)[, "Q97.5"]/seeds)
-
-# figures
-mvFungFig <- ggplot(mvGermD1Dat, aes(treatment, prop_dark)) +
-    # geom_point(alpha = 0.3, size = 0.7, position = position_jitter(width = 0.35)) +
-  # geom_violin() +
-  ggdist::stat_dots(dotsize = .4, 
-                    binwidth = 0.01,
-                    color = "#238A8DFF",
-                    fill = "#238A8DFF",
-                    alpha = 0.7,
-                    side = "both") +
-    geom_errorbar(data = mvFungSim, width = 0, size = 0.25, aes(ymin = lower, ymax = upper)) +
-    geom_point(data = mvFungSim, size = 1.25) +
+# figure
+ggplot(germDraws, aes(x = sp, y = fung_eff)) +
+  geom_hline(yintercept = 0) +
+  geom_violin(fill = NA) +
+  labs(y = "Fungicide effect on germination") +
   fig_theme +
-  theme(axis.title.x = element_blank()) +
-  ylab("Infected fraction")
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(face = "italic"))
 
-mvGermFig <- ggplot(mvGermD1Dat, aes(prop_dark, prop_germ)) +
-  geom_point(alpha = 0.3, size = 0.7, color = "#238A8DFF") +
-  geom_line(data = mvGermSim) +
-  geom_ribbon(data = mvGermSim, aes(ymin = lower, ymax = upper), alpha = 0.4) +
+
+#### seed infection figure ####
+
+# posterior draws
+mvGermInfD1Draws <- as_draws_df(mvGermInfD1Mod)
+mvPropDarkDraws <- as_draws_df(mvPropDarkMod)
+mvPropLightDraws <- as_draws_df(mvPropLightMod)
+
+# combine
+infDraws <- tibble(fungi = "dark",
+                   response = "Infection effect on germination",
+                   eff = mvGermInfD1Draws$b_prop_dark) %>%
+  full_join(tibble(fungi = "light",
+                   response = "Infection effect on germination",
+                   eff = mvGermInfD1Draws$b_prop_light)) %>%
+  full_join(tibble(fungi = "dark",
+                   response = "Fungicide effect on infection",
+                   eff = mvPropDarkDraws$b_fungicide) %>%
+              full_join(tibble(fungi = "light",
+                               response = "Fungicide effect on infection",
+                               eff = mvPropLightDraws$b_fungicide)))
+
+# figure
+ggplot(infDraws, aes(x = fungi, y = eff)) +
+  geom_hline(yintercept = 0) +
+  geom_violin(fill = NA) +
+  facet_wrap(~ response, strip.position = "left",
+             scales = "free") +
+  labs(x = "Seed fungi color") +
   fig_theme +
-  xlab("Infected fraction") +
-  ylab("Germination fraction")
-
-pdf("output/mv_germination_infection_figure_2018_density_exp.pdf", width = 3.54, height = 1.57)
-plot_grid(mvFungFig, mvGermFig,
-          nrow = 1,
-          labels = LETTERS[1:2],
-          label_size = 10)
-dev.off()
+  theme(axis.title.y = element_blank())
 
 
-#### values for text ####
+#### START HERE: values for text ####
+
+# fungicide effects
+mean_hdci(evGermDraws$b_fungicide)
 
 # Mv fungicide effect on infection
-hypothesis(mvPropDarkMod2, "plogis(Intercept + fungicide) - plogis(Intercept) = 0")
+hypothesis(mvPropDarkMod, "plogis(Intercept + fungicide) - plogis(Intercept) = 0")
 
 # Mv infection effect on germination
 filter(mvGermSim, prop_dark == min(prop_dark))
